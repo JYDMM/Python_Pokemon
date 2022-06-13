035e25cf3fb6edff6972d1c3108cbb19cfc67987
a <a@A.com>
2022-06-13 10:32:20 -0400

    Fixed stuff that I screwed up in last commit

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from sprites import player
 pygame.init()
 
 size = width, height = 320 * 4, 240 * 4
-speed = [1, 1]
 black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
@@ -124,17 +123,3 @@ while run:
             sys.exit(69)
 
     redraw_game_window()
-
-# while 1:
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT: sys.exit()
-#
-#     ballrect = ballrect.move(speed)
-#     if ballrect.left < 0 or ballrect.right > width:
-#         speed[0] = -speed[0]
-#     if ballrect.top < 0 or ballrect.bottom > height:
-#         speed[1] = -speed[1]
-#
-#     screen.fill(black)
-#     screen.blit(ball, ballrect)
-#     pygame.display.flip()
